8e3b27fe0325606b9e06176f3fc6887280afa762
Lukman350 <nulhakiml725@gmail.com>
2023-01-09 20:01:46 +0700

    bug fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,6 @@
 import '../css/input.css';
-import { SerialPort } from 'serialport';
 
 const channelNumberVHF = [
   '#channel-number-1',
   '#channel-number-2'
 ];
-
-const port = new SerialPort({
-  path: '',
-  baudRate: 57600,
-});
-
-// port.on('readable', () => {
-//   console.log('Data: ', port.read());
-// });
